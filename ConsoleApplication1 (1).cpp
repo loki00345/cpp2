476f1eea8e119b280019ee880111eb336e13114a
loki00345 <99328513+loki00345@users.noreply.github.com>
2024-05-31 23:39:05 +0300

    Add files via upload

---
+++ ConsoleApplication1 (1).cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*ex1*/
+
+    /*const float R1 = 4;
+    const float R2 = 6;
+    const float R3 = 8;
+    const float R0 = 1 / (1 / R1 + 1 / R2 + 1 / R3);
+    cout << "znachenya oporu: "<< R0 << endl;*/
+
+    /*ex2*/
+
+    /*int v, t, a;
+    cout << "v: ";
+    cin >> v;
+    cout << "t: ";
+    cin >> t;
+    cout << "a: ";
+    cin >> a;
+
+    int distance = v * t + (a * t * 2) / 2;
+
+    cout << "distance: " << distance << endl;*/
+    
+    /*ex3*/
+
+    /*int Seconds;
+    cout << "enter sec:  ";
+    cin >> Seconds;
+
+
+    int hours = Seconds / 3600;
+    int minutes = (Seconds % 3600) / 60;
+    int seconds = Seconds % 60;
+
+
+    cout << "hours: " << hours << endl;
+    cout << "min: " << minutes << endl;
+    cout << "sec: " << seconds << endl;*/
+
+    /*ex4*/
+    /*double inputNumber;
+    cout << "enter num: ";
+    cin >> inputNumber;
+
+
+    int hryvnia = (inputNumber);
+    int kopecks = (inputNumber - hryvnia) * 100;
+
+    cout << "format: " << hryvnia << " grn " << kopecks << " cop." << endl;*/
+    /*ex5*/
+    /*double distanceMeters;
+    cout << "meters: ";
+    cin >> distanceMeters;
+
+
+    double timeMinutes, timeSeconds;
+    cout << "min sec: ";
+    cin >> timeMinutes >> timeSeconds;
+
+
+    double totalTimeSeconds = timeMinutes * 60 + timeSeconds;
+
+
+    double speedKmPerHour = (distanceMeters / 1000) / (totalTimeSeconds / 3600);
+
+
+    cout << "\nmeters: " << distanceMeters << " m;\n";
+    cout << "time: " << timeMinutes << " min " << timeSeconds << " sec = " << totalTimeSeconds << " ces;\n";
+    cout << "km/hour " << speedKmPerHour << " km/hour.\n";*/
+    /*ex6*/
+    /*int days;
+
+    cout << "Enter the number of days: ";
+    cin >> days;
+
+    int weeks = days / 7;
+    int remainingDays = days % 7;
+
+    cout << "results: " << weeks << " weeks and " << remainingDays << " days" << endl;
+}*/
